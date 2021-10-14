3c439324ec7b7812de8a1b06576518f35d73d010
Atmos945 <92547087+Atmos945@users.noreply.github.com>
2021-10-14 21:52:40 +0200

    Add files via upload

---
+++ config dell nsi.ps1
@@ -0,0 +1,50 @@
+winget install LibreOffice.LibreOffice
+winget install Mozilla.Firefox
+winget install Python.Python.3
+winget install V.MAILLE.EduPython
+winget install Microsoft.PowerShell
+winget install Microsoft.VisualStudioCode
+winget install ONLYOFFICE.DesktopEditors
+winget install Meltytech.Shotcut
+winget install KDE.Kdenlive
+winget install GIMP.GIMP
+winget install Microsoft.PowerToys
+winget install ModernFlyouts.ModernFlyouts
+winget install Zoom.Zoom
+
+
+Write-Host "
+Google Docs :
+Extension : https://chrome.google.com/webstore/detail/docs/aohghmighlieiainnegkcijnfilokake?hl=fr
+Site : https://docs.google.com/document/u/0/
+
+Google Sheets :
+Extension : https://chrome.google.com/webstore/detail/sheets/felcaaldnbdncclmgdcncolpebgiejap
+Site : https://docs.google.com/spreadsheets/u/0/
+
+Google Slides :
+Extension : https://chrome.google.com/webstore/detail/slides/aapocclcgogkmnckokdopfmhonfmgoek
+Site : https://docs.google.com/presentation/u/0/
+
+Youtube : 
+Site :https://youtube.com
+
+XAML Controls Gallery : 
+Store : https://www.microsoft.com/store/productId/9MSVH128X2ZT
+
+Jitsi Meet :
+WGTPKG : Jitsi.Meet
+Site : https://meet.jit.si/
+
+
+
+
+liens :
+pronote
+pinterest
+
+
+desac onedrive
+google moteur par défaut + config navigateurs
+sandbox et guardian
+"
